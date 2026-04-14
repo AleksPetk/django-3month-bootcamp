4e9400d8d12e9fe60c3ff0273e8237c18c2c38fe
Aleksandar Petkov <alekspetk@Aleksandars-MacBook-Air.local>
2026-04-14 19:42:40 +0900

    Week 2 day 2 done

--- Week 2 HTTP Basic, Get, Post/pages/urls.py
+++ Week 2 HTTP Basic, Get, Post/pages/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("hello_get/", views.hello_get, name="hello_get"),
     path("search/", views.search, name="search"),
+    path("hello_post/", views.hello_post, name="hello_post"),
+    path("hello_result/", views.hello_result, name="hello_result"),
 ]
--- Week 2 HTTP Basic, Get, Post/pages/views.py
+++ Week 2 HTTP Basic, Get, Post/pages/views.py
@@ -1,6 +1,6 @@
 
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -21,6 +21,56 @@ def hello_get(request):
         "city": city.capitalize(),
         "food": food
     })
+
+def hello_post(request):
+    name = None
+    age = None
+    city = None
+    food = None
+    method = "GET"
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+        city = request.POST.get("city")
+        food = request.POST.get("food")
+        method = "POST"
+
+        return redirect(f"/hello_result/?success=1&name={name}&age={age}&city={city}&food={food}")
+
+    return render(request, "hello_post.html")
+
+
+
+def hello_result(request):
+    name = request.GET.get("name", "Guest")
+    age = request.GET.get("age", "No Age")
+    city = request.GET.get("city", "City")
+    food = request.GET.get("food", "Sushi")
+    success = request.GET.get("success")
+    if success == "1":
+
+        if food:
+            food.capitalize()
+        else:
+            food = "No food added"
+
+        if age.isdigit():
+            age = int(age)
+        else:
+            age = "No proper age added"
+
+        return render(request, "hello_result.html", {
+            "data": {
+                "Success":"You successfuly Submit",
+                "Name": name.capitalize(),
+                "Age": age,
+                "City":city.capitalize(),
+                "Food":food
+            }
+        })
+
+
 
 def search(request):
     search_for = request.GET.get("search_for")
